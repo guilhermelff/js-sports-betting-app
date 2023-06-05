5b76660bab48df1d4dbfa756e809b2c651233010
Guilherme Lopes <guilhermelff@id.uff.br>
2023-06-04 23:11:27 -0300

    notificações e ajustes

--- script.js
+++ script.js
@@ -50,3 +50,10 @@ function includeHTML() {
         }
     }
 }
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+})
