84cee2e83774c7a432f40d4a49c1f93a96f0b380
Guilherme Lopes <guilhermelff@id.uff.br>
2023-07-21 01:34:52 -0300

    instalando node e conectando ao supabase

--- script.js
+++ script.js
@@ -68,3 +68,8 @@ function mostrar() {
     document.getElementById('expandir').style.display = 'none';
 }
 
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://qmkgsptmjfsyocbglppl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFta2dzcHRtamZzeW9jYmdscHBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODg1NjQ2NjksImV4cCI6MjAwNDE0MDY2OX0.0b0azCaqSYJR8qi3o5lglzjRAoPEDH11ALL3OHaSsgk')
